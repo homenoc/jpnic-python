6fa9b7117cc476f28161615c62a7dd9f73e2d2fe
yoneyan <github@yoneyan.net>
2022-01-07 14:35:40 +0900

    [support] Supported getting ipv6 information.

--- jpnic/jpnic.py
+++ jpnic/jpnic.py
@@ -80,3 +80,72 @@ class JPNIC:
             count += 1
 
         return headers, tables
+
+    def get_ipv6(self, data_filter):
+        menu_name = '登録情報検索(IPv6)'
+        try:
+            menu_url, s = base.init_access(self.base_url, self.ca_path, self.cert_path, self.key_path, menu_name)
+        except Exception as e:
+            raise e
+        r = s.get(self.base_url + '/jpnic/' + menu_url)
+        # auto encode
+        r.encoding = r.apparent_encoding
+        print(r.status_code)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        submit_url = soup.find('form')['action']
+        dest_disp = soup.find('input', attrs={'name': 'destdisp'})['value']
+
+        json = {
+            'org.apache.struts.taglib.html.TOKEN': base.get_random(32),
+            'destdisp': dest_disp,
+            'ipaddr': data_filter['ip_address'],  # IPアドレス
+            'sizeS': data_filter['size_start'],  # サイズ (開始)
+            'sizeE': data_filter['size_end'],  # サイズ (終了)
+            'netwrkName': data_filter['network_name'],  # ネットワーク名
+            'regDateS': data_filter['reg_start'],  # 割振・割当年月日(開始)
+            'regDateE': data_filter['reg_end'],  # 割振・割当年月日(終了)
+            'rtnDateS': data_filter['return_start'],  # 返却年月日(開始)
+            'rtnDateE': data_filter['return_end'],  # 返却年月日(終了)
+            'organizationName': data_filter['org_name'],  # 組織名
+            'resceAdmSnm': data_filter['resource_admin_short'],  # 資源管理者略称
+            'recepNo': data_filter['recep_number'],  # 受付番号
+            'deliNo': data_filter['deli_number'],  # 審議番号
+            'regKindAllo': data_filter['is_allocate'],  # 割り振り
+            'regKindEvent': data_filter['is_assign_infra'],  # インフラ割当
+            'regKindUser': data_filter['is_assign_user'],  # ユーザ割当
+            'regKindSubA': data_filter['is_sub_allocate'],  # SUBA
+            'action': '%81%40%8C%9F%8D%F5%81%40',
+        }
+
+        print(self.base_url + '/' + submit_url)
+        r = s.post(
+            self.base_url + submit_url,
+            data=json,
+        )
+        # auto encode
+        r.encoding = r.apparent_encoding
+        # print(r.status_code)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        # print(soup.findAll('td', attrs={'class': 'dataRow_mnt04'}))
+        count, all_count = 0, 0
+        all_count = 0
+        headers = []
+        tables = []
+        tmp_row = []
+
+        for i in soup.findAll('td', attrs={'class': 'dataRow_mnt04'}):
+            if count == 9:
+                count = 0
+                if all_count > 0:
+                    tables.append(tmp_row)
+                    tmp_row = []
+                all_count += 1
+
+            if all_count == 0:
+                headers.append(i.text.strip())
+            else:
+                tmp_row.append(i.text.strip())
+
+            count += 1
+
+        return headers, tables
--- test_jpnic.py
+++ test_jpnic.py
@@ -39,19 +39,60 @@ class TestJPNIC(unittest.TestCase):
             'return_start': '',
             'return_end': '',
             'org_name': '',
-            'resource_admin_short': 'SAKURA',
+            'resource_admin_short': 'HOMENOC',
             'recep_number': '',
             'deli_number': '',
-            'is_pa': 'on',  # PA
+            'is_pa': 'off',  # PA
             'is_assign': 'on',  # 割り振り
-            'is_allocate_infra': 'on',  # インフラ割当
-            'is_allocate_user': 'on',  # ユーザ割当
-            'is_suba': 'on',  # SUBA
-            'is_historical_pi': 'on',  # 歴史的PI
-            'is_special_pi': 'on',  # 特殊用途PI
+            'is_allocate_infra': 'off',  # インフラ割当
+            'is_allocate_user': 'off',  # ユーザ割当
+            'is_suba': 'off',  # SUBA
+            'is_historical_pi': 'off',  # 歴史的PI
+            'is_special_pi': 'off',  # 特殊用途PI
+            # 'is_pa': 'on',  # PA
+            # 'is_assign': 'on',  # 割り振り
+            # 'is_allocate_infra': 'on',  # インフラ割当
+            # 'is_allocate_user': 'on',  # ユーザ割当
+            # 'is_suba': 'on',  # SUBA
+            # 'is_historical_pi': 'on',  # 歴史的PI
+            # 'is_special_pi': 'on',  # 特殊用途PI
         }
 
         headers, table = j.get_ipv4(data_filter)
+        result = tabulate(table, headers, tablefmt="grid")
+        print(result)
+
+    def get_ipv6(self):
+        base_url = 'https://iphostmaster.nic.ad.jp'
+        ca_path = '/Users/y-yoneda/github/homenoc/jpnic-python/cert/rootcacert_r3.cer'
+        cert_path = '/Users/y-yoneda/github/homenoc/jpnic-python/cert/v6-cert.pem'
+        key_path = '/Users/y-yoneda/github/homenoc/jpnic-python/cert/v6-prvkey.pem'
+        j = JPNIC()
+        j.base_url = base_url
+        j.ca_path = ca_path
+        j.cert_path = cert_path
+        j.key_path = key_path
+
+        data_filter = {
+            'ip_address': '',
+            'size_start': '',
+            'size_end': '',
+            'network_name': '',
+            'reg_start': '',
+            'reg_end': '',
+            'return_start': '',
+            'return_end': '',
+            'org_name': '',
+            'resource_admin_short': 'doornoc',
+            'recep_number': '',
+            'deli_number': '',
+            'is_allocate': 'on',  # 割振
+            'is_assign_infra': 'on',  # インフラ割当
+            'is_assign_user': 'on',  # ユーザ割当
+            'is_sub_allocate': 'on',  # 再割当
+        }
+
+        headers, table = j.get_ipv6(data_filter)
         result = tabulate(table, headers, tablefmt="grid")
         print(result)
 
